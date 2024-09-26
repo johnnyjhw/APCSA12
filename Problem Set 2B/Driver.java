e8152f7c027bc9cc6c1e35c5d07867df358ddcf8
johnnyjhw <johnnywang0208@gmail.com>
2024-09-26 09:12:06 -0700

    Add files via upload

---
+++ Problem Set 2B/Driver.java
@@ -0,0 +1,45 @@
+
+import java.util.Scanner;
+
+class Driver {
+
+   public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    StringTools sTools = new StringTools();
+
+    System.out.println("lastLetter test cases: ");
+    System.out.println("Enter a word with at least one character: ");
+    String answer1 = input.nextLine();
+    System.out.println("The last letter is " + sTools.lastletter(answer1));
+    System.out.println("---------------------");
+    System.out.println("");
+    
+    System.out.println("Phone number format: ");
+    System.out.println("Enter a phone number: ");
+    String answer2 = input.nextLine();
+    System.out.println("Phone number format: " + sTools.formatPhoneNumber(answer2));
+    System.out.println("---------------------");
+    System.out.println("");
+      
+      
+    System.out.println("Phone number format: ");
+    System.out.println("Enter a word: ");
+    String answer3 = input.nextLine();
+    System.out.println("The middle three are: " + sTools.middleThree(answer3));
+    System.out.println("---------------------");
+    
+    System.out.println("");
+    System.out.println("Enter a word: ");
+    String answer4 = input.nextLine();
+    System.out.println("The last two letters swapped is: " + sTools.swapLastTwo(answer4));
+    System.out.println("---------------------");
+    
+    
+    System.out.println("");
+    System.out.println("Enter a word: ");
+    String answer5 = input.nextLine();
+    int answerint = input.nextInt();
+    System.out.println("The result: " + sTools.frontAgain(answer5, answerint));
+    
+   }
+}
