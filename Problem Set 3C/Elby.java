62ea9006412c25cd80d971c1a733b920867ba216
johnnyjhw <johnnywang0208@gmail.com>
2024-10-14 12:26:03 -0700

    Update Elby.java

--- Problem Set 3C/Elby.java
+++ Problem Set 3C/Elby.java
@@ -1,65 +1,48 @@
 public class Elby {
-    /**
-     * Get a default greeting
-     *
-     * @return a greeting
-     */
+
     public String getGreeting() {
         return "Hello, let's talk.";
     }
 
-    /**
-     * Gives a response to a user statement
-     *
-     * @param statement the user statement
-     * @return a response based on the rules given
-     */
+
     public String getResponse(String statement) {
-        int index - findKeyward(statement, "hello", 0);
-        if (index != -1) {
-            return "Hello! How can I help you today?";
-        } else {
-            return "I'm not sure what you mean.";
-        
         String response = "";
 
-        if (statement.indexOf("mother") >= 0
-        || statement.indexOf("father") >= 0
-        || statement.indexOf("sister") >= 0
-        || statement.indexOf("brother") >= 0) {
+        if (findKeyword(statement, "I want to", 0) >= 0) {
+            response = transformIWantToStatement(statement);
+        } else if (findKeyword(statement, "you", 0) >= 0
+                && findKeyword(statement, "me", findKeyword(statement, "you", 0)) >= 0) {
+            response = transformYouMeStatement(statement);
+        } else if (findKeyword(statement, "mother", 0) >= 0
+                || findKeyword(statement, "father", 0) >= 0
+                || findKeyword(statement, "sister", 0) >= 0
+                || findKeyword(statement, "brother", 0) >= 0) {
             response = "Tell me more about your family.";
-        } else if (statement.indexOf("no"){
-            reponse = "Why so serious?";
-        }
-        else if (statement.indexOf("dog") >= 0
-        || statement.indexOf("cat") >= 0
-        ) {
-            response = "Tell me more about your pets. ";
-        } else if (statement.indexOf("Johnny") >= 0) {
-            response = "He is a good student";
+        } else if (findKeyword(statement, "no", 0) >= 0) {
+            response = "Why so serious?";
+        } else if (findKeyword(statement, "dog", 0) >= 0
+                || findKeyword(statement, "cat", 0) >= 0) {
+            response = "Tell me more about your pets.";
+        } else if (findKeyword(statement, "Johnny", 0) >= 0) {
+            response = "He is a good student.";
         } else if (statement.trim().length() == 0) {
-            response = "Say something, please";
-        } else if (statement.indexOf("sad") >= 0) {
-            response = "I hope you feel better soon";
-        } else if (statement.indexOf("depressed") >= 0) {
-            response = "You should talk to a therapist";
-        } else if (statement.indexOf("pizza") >= 0) {
-            response = "I LOVE CHEESE";
-        }
-        else {
+            response = "Say something, please.";
+        } else if (findKeyword(statement, "sad", 0) >= 0) {
+            response = "I hope you feel better soon.";
+        } else if (findKeyword(statement, "depressed", 0) >= 0) {
+            response = "You should talk to a therapist.";
+        } else if (findKeyword(statement, "pizza", 0) >= 0) {
+            response = "I LOVE CHEESE!";
+        } else {
             response = getRandomResponse();
         }
 
         return response;
     }
 
-    /**
-     * Pick a default response to use if nothing else fits.
-     *
-     * @return a non-committal string
-     */
+
     private String getRandomResponse() {
-        final int NUMBER_OF_RESPONSES = 5;
+        final int NUMBER_OF_RESPONSES = 6;
         double r = Math.random();
         int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 
@@ -76,11 +59,8 @@ public class Elby {
         } else if (whichResponse == 4) {
             response = "Bro what?";
         } else if (whichResponse == 5) {
-            reponse = "I see...";
-        } else if (whichResponse == 6) {
-            reponse = "Why does this bother you?";
+            response = "I see...";
         }
-
         return response;
     }
 
@@ -90,26 +70,65 @@ public class Elby {
         goal = goal.toLowerCase();
 
         int goalPos = phrase.indexOf(goal, startPos);
+
+
         while (goalPos >= 0) {
             String before = " ", after = " ";
+
             if (goalPos > 0) {
                 before = phrase.substring(goalPos - 1, goalPos);
             }
 
             if (goalPos + goal.length() < phrase.length()) {
-                after = phrase.substring(goalPos + goal.length(), 
-                    goalPos + goal.length() + 1);
+                after = phrase.substring(goalPos + goal.length(),
+                        goalPos + goal.length() + 1);
             }
 
-      
-            if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) &&                
-            ((after.compareTo("a")  < 0) || (after.compareTo("z")  > 0))) {
+
+            if (((before.compareTo("a") < 0) || (before.compareTo("z") > 0)) &&
+                ((after.compareTo("a") < 0) || (after.compareTo("z") > 0))) {
                 return goalPos;
             }
 
-            goalPos = phrase.indexOf(goal,goalPos + 1);
+ 
+            goalPos = phrase.indexOf(goal, goalPos + 1);
         }
+
         return -1;
     }
+
+    /**
+     * Take a statement with "you <something> me" and transform it into "What makes
+     * you think that I <something> you?"
+     *
+     * @param statement the user statement, assumed to contain "you" followed by "me"
+     * @return the transformed statement
+     */
+    public String transformYouMeStatement(String statement) {
+       
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+
+        int posOfYou = findKeyword(statement, "you", 0);
+        int posOfMe = findKeyword(statement, "me", posOfYou + 3);
+
+        String restOfStatement = statement.substring(posOfYou + 3, posOfMe).trim();
+        return "What makes you think that I " + restOfStatement + " you?";
+    }
+
+
+    public String transformIWantToStatement(String statement) {
+       
+        statement = statement.trim();
+        String lastChar = statement.substring(statement.length() - 1);
+        if (lastChar.equals(".")) {
+            statement = statement.substring(0, statement.length() - 1);
+        }
+        int pos = findKeyword(statement, "I want to", 0);
+        String restOfStatement = statement.substring(pos + 9).trim();
+        return "What would it mean to " + restOfStatement + "?";
+    }
 }
- 
