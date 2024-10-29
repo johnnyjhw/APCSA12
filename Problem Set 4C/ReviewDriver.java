2a375d4744bf9b0e0a1fe3a9fb29e5dca864e951
johnnyjhw <johnnywang0208@gmail.com>
2024-10-28 19:13:46 -0700

    Add files via upload

---
+++ Problem Set 4C/ReviewDriver.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class ReviewDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class ReviewDriver
+{
+    public static void main(String[] args) {
+        double a = Review.sentimentVal("approved");
+        System.out.println(a);
+    }
+    }
+
