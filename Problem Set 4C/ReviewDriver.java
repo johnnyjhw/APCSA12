284de7bea31188e59986a47425876246e9b541ec
johnnyjhw <johnnywang0208@gmail.com>
2024-10-30 13:35:45 -0700

    Update ReviewDriver.java

--- Problem Set 4C/ReviewDriver.java
+++ Problem Set 4C/ReviewDriver.java
@@ -8,17 +8,31 @@
 public class ReviewDriver
 {
     public static void main(String[] args) {
-        
-        double a = Review.sentimentVal("approved.txt");
+        /*
+        double a = Review.sentimentVal("approved");
         System.out.println(a);
+        
+        double b = Review.sentimentVal("dream");
+        System.out.println(b);
+        
+        double c = Review.sentimentVal("reality");
+        System.out.println(c);
+        
+        */
         double simplesenti = Review.totalSentiment("SimpleReview.txt");
         System.out.println(simplesenti);
-    
+        
         double lbssreview = Review.totalSentiment("lbreview.txt");
         System.out.println(lbssreview);
         
         double teststar = Review.starRating("lbreview.txt");
         System.out.println(teststar);
+        
+        String fakereview = Review.fakeReview("SimpleReview.txt");
+        System.out.println(fakereview);
+        
+        String fakereview2 = Review.fakeReview("lbReview.txt");
+        System.out.println(fakereview2);
     
     }
     }
